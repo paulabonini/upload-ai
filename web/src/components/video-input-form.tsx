f214347ec67e7b483663f7274c4c6041f9a8b38b
Paula Bonini <paula_almeidabonini@hotmail.com>
2023-09-14 09:51:22 -0300

    ffmpeg usage

--- web/src/components/video-input-form.tsx
+++ web/src/components/video-input-form.tsx
@@ -4,6 +4,8 @@ import { Separator } from './ui/separator'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from 'react'
+import { getFFmpeg } from '@/lib/ffmpeg'
+import { fetchFile } from '@ffmpeg/util'
 
 export function VideoInputForm() {
   const [videoFile, setVideoFile] = useState<File | null>(null)
@@ -21,7 +23,46 @@ export function VideoInputForm() {
     setVideoFile(selectedFile)
   }
 
-  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+  async function convertVideoToAudio(video: File) {
+    console.log('ok')
+
+    const ffmpeg = await getFFmpeg()
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+
+    // ffmpeg.on('log', log => {
+    //   console.log(log)
+    // })
+
+    ffmpeg.on('progress', (progress) => {
+      console.log('Convert progress: ' + Math.round(progress.progress * 100))
+    })
+
+    await ffmpeg.exec([
+      '-i',
+      'input.mp4',
+      '-map',
+      '0:a',
+      '-b:a',
+      '20k',
+      '-acodec',
+      'libmp3lame',
+      'output.mp3',
+    ])
+
+    const data = await ffmpeg.readFile('output.mp3')
+
+    const audioFileBlob = new Blob([data], { type: ' audio/mpeg' })
+    const audioFile = new File([audioFileBlob], 'audio.nmp3', {
+      type: 'audio/mpeg',
+    })
+
+    console.log('end')
+
+    return audioFile
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
     const prompt = promptInputRef.current?.value
@@ -29,6 +70,10 @@ export function VideoInputForm() {
     if (!videoFile) {
       return
     }
+
+    const audioFile = await convertVideoToAudio(videoFile)
+
+    console.log(audioFile)
   }
 
   const previewURL = useMemo(() => {
